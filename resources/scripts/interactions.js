2b9e1b82b3e56f6dc96e8cc0ecb284cc29a65481
Peterson Senha <peterson.senha@gmail.com>
2022-08-13 21:59:34 -0300

    Ajustes Finais

--- resources/scripts/interactions.js
+++ resources/scripts/interactions.js
@@ -1,9 +1,13 @@
-function mudarVisibilidade(bloco, instrucao) {
+function mudarVisibilidade(bloco, instrucao, box) {
   if (bloco.style.visibility === "hidden") {
     bloco.style.visibility = "visible";
+    box.style.background = "rgb(222,227,220,1)";
+    bloco.style.background = "rgb(222,227,220,1)";
     instrucao ? (instrucao.style.visibility = "hidden") : "";
   } else {
     bloco.style.visibility = "hidden";
+    bloco.style.background = "rgb(255,255,255)";
+    box.style.background = "rgb(255,255,255)";
     instrucao ? (instrucao.style.visibility = "visible") : "";
   }
 }
